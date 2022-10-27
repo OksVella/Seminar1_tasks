470f406b9f875445faf386bd6f592a7a4a3a397f
Ovella <oks.vella@yandex.ru>
2022-10-28 01:23:29 +0300

    added task number 1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = 9;
+int num2 = 17;
+int max = 0;
+int min = 0;
+if (num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num2;
+    min = num1;
+}
+Console.WriteLine("Min = " + min);
+Console.WriteLine("Max = " + max);
+

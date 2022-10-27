c515220d8a7c40ee4deb8b405083c8cc46c14b92
Ovella <oks.vella@yandex.ru>
2022-10-28 02:00:33 +0300

    added task 2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = 3;
+int num2 = 7;
+int num3 = 5;
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("Maximum = " + max);

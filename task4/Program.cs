07b55a866d605524bb3d43d6af3bbe4f380c39e8
Ovella <oks.vella@yandex.ru>
2022-10-28 03:08:34 +0300

    added task 4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿int N = 10;
+int Startnumber = 1;
+while(Startnumber <= N)
+{
+    if(Startnumber % 2 == 0)
+    {
+        Console.Write(Startnumber);
+    }
+    Startnumber = Startnumber + 1;
+}
